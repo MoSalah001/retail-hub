92dc9cd492a95f48e68fd0ccc38d7071363a444d
MoSalah001 <mhmdsalah707@gmail.com>
2023-06-20 03:08:26 +0300

    auto login

--- Client/js/index.js
+++ Client/js/index.js
@@ -33,3 +33,28 @@ function signIn(e){
         window.location.assign(xhr.responseURL)
     }    
 }
+
+window.onload = ()=>{
+    function checkLoggedUser(){     //check if user is logged
+        const user = document.cookie.slice('5')
+        if(user.length < 2){
+            window.location.assign(window.origin);
+        } else  {
+            const xhr = new XMLHttpRequest()
+            xhr.open('post','/check')
+            xhr.setRequestHeader('content-type','application/json')
+            xhr.send(JSON.stringify({
+                user : user
+            }))
+            xhr.onload = ()=>{
+                if(xhr.status !== 200) {
+                    window.location.assign(window.origin)
+                } else {
+                    window.location.href = '/main.html'
+                }
+            }
+        }
+    }    
+    checkLoggedUser();
+}
+
